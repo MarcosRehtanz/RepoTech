a88726f979dcde49be28382ab961e0dd0b69bcc9
Mansilla <marcos.rehtanz@gmail.com>
2023-09-23 23:44:09 -0300

    Feat(graphics): Add graphics

---
+++ src/app/graphics/@bargraph/page.jsx
@@ -0,0 +1,17 @@
+'use client'
+import { GraphBar } from "@/components/Graphics/Graphics"
+
+const BarGraph = () => {
+
+    return (
+        <>
+            <h1>Bar Graph</h1>
+            <div>
+                <GraphBar />
+            </div>
+        </>
+    )
+
+}
+
+export default BarGraph
---
+++ src/app/graphics/layout.jsx
@@ -0,0 +1,13 @@
+
+const GraphicsLayout = ({bargraph, linegraph}) => {
+
+    return(
+        <main>
+            {bargraph}
+            {linegraph}
+        </main>
+    )
+
+}
+
+export default GraphicsLayout
